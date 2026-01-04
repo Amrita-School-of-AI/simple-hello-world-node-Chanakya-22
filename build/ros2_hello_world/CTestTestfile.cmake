fcc013b60d0758927ca13ba70a9a963f34bf2543
Chanakya-22 <chanakya.jarubula@gmail.com>
2026-01-04 23:22:15 +0530

    Remove build artifacts and add gitignore

--- build/ros2_hello_world/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chanakya/simple-hello-world-node-Chanakya-22
-# Build directory: /home/chanakya/simple-hello-world-node-Chanakya-22/build/ros2_hello_world
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
